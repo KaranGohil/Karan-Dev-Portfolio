8fec4d397b826f12daa6e5d61afc2bcb869826fc
KaranGohil <karandilipsinhgohil@gmail.com>
2023-09-22 18:58:28 -0700

    added credit in the end

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,28 @@ const App: React.FC = () => {
 			<AboutMe />
 			<Experience />
 			<ProjectSection />
+			<div className="credit">
+				<p>
+					Design inspired by{' '}
+					<a
+						href="https://www.youtube.com/@Hyperplexed"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Hyperplexed
+					</a>{' '}
+					and{' '}
+					<a
+						href="https://brittanychiang.com/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Brittany Chiang
+					</a>
+					. Built with React, Vite, Typescript and Framer Motion. Deployed with
+					Netlify.
+				</p>
+			</div>
 		</>
 	);
 };
